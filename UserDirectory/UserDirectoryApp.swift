8d0866bdadd24aaed6ea4d75acb9e89e5daf3406
Bakr98 <bmarouf98@gmail.com>
2025-06-17 00:34:53 -0400

    Initial Commit

---
+++ UserDirectory/UserDirectoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDirectoryApp.swift
+//  UserDirectory
+//
+//  Created by Bakr Marouf on 6/17/25.
+//
+
+import SwiftUI
+
+@main
+struct UserDirectoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
